0260ad97569d3e5e4b5aaeb92c6f4e67edb1ac21
Patrick Johnston <gcupcakekid@gmail.com>
2025-04-03 01:09:27 +0100

    Implement default ctor. Add non-char support for formatter specialisation. Dup tests for wzstring_view. Minor fixes (#1)

--- examples/example.cpp
+++ examples/example.cpp
@@ -25,6 +25,7 @@ int main() {
     std::string s = "hello world";
     std::zstring_view z0 = "hello";
     std::zstring_view z1 = s;
+    std::zstring_view empty;
     std::println("{}", z0);
     std::println("{}", s.starts_with(z0));
     std::println("{}", z0.starts_with(s));
@@ -34,9 +35,36 @@ int main() {
     std::cout<<z1<<std::endl;
     std::println("{}", "hello"zsv == "hello"sv);
     std::println("{}", "hello"zsv == "hello"zsv);
+    std::println("{}", "hello"zsv != "goodbye"sv);
+    std::println("{}", "hello"zsv != "goodbye"zsv);
     std::println("{}", z0 == z1);
     std::println("{}", to_string(z0 <=> z1));
     std::println("{}", z0[z0.size()] * 1);
     std::println("{}", z0.c_str());
+    std::println("\"{}\"", empty);
+    std::println("{}", empty == ""zsv);
     std::formatter<std::string_view> f;
+    
+    std::wstring ws = L"hello world";
+    std::wzstring_view wz0 = L"hello";
+    std::wzstring_view wz1 = ws;
+    std::wzstring_view wempty;
+    std::wcout << std::format(L"{}\n", wz0);
+    std::println("{}", ws.starts_with(wz0));
+    std::println("{}", wz0.starts_with(ws));
+    std::println("{}", wz0.starts_with(L"hello"));
+    std::println("{}", wz0.starts_with(L"hello"zsv));
+    std::println("{}", std::hash<std::wzstring_view>{}(wz1));
+    std::wcout<<wz1<<std::endl;
+    std::println("{}", L"hello"zsv == L"hello"sv);
+    std::println("{}", L"hello"zsv == L"hello"zsv);
+    std::println("{}", L"hello"zsv != L"goodbye"sv);
+    std::println("{}", L"hello"zsv != L"goodbye"zsv);
+    std::println("{}", wz0 == wz1);
+    std::println("{}", to_string(wz0 <=> wz1));
+    std::println("{}", wz0[wz0.size()] * 1);
+    std::wcout << std::format(L"{}\n", wz0.c_str());
+    std::wcout << std::format(L"\"{}\"\n", wempty);
+    std::println("{}", wempty == L""zsv);
+    std::formatter<std::wstring_view, wchar_t> wf;
 }
